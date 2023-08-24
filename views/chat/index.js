513099f8127d9c39a9ab2f22f65bf2046ce0e060
Radex <radexgamer13@gmail.com>
2023-08-23 20:12:19 -0400

    spinner chat fadein

--- views/chat/index.js
+++ views/chat/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const responseUserid = await axios.patch('/api/userslobby');
   socket.emit('setUserId', responseUserid.data);
   const chatsContent = document.getElementById('select-content');
+  const chatLoading = document.getElementById('select-loading')
   const responseInvitation = await axios.get('/api/invitations/chat');
   const pendingInvitations = responseInvitation.data.filter(invitation => invitation.status === 'accepted');
   const findId = await axios.patch('/api/userslobby');
@@ -20,8 +21,15 @@ document.addEventListener('DOMContentLoaded', async () => {
     const chatItem = document.createElement('div');
     chatItem.id = userSend;
     chatItem.setAttribute('class', 'p-2 cursor-pointer chat-item');
-    chatItem.innerHTML = createChatProfile(userData);
-    chatsContent.appendChild(chatItem);
+    chatLoading.classList.add('fadeInn');
+    setTimeout(() => {
+      chatLoading.classList.remove('flex');
+      chatLoading.classList.add('hidden');
+      chatItem.innerHTML = createChatProfile(userData);
+      chatsContent.classList.remove('hidden');
+      chatsContent.classList.add('flex');
+      chatsContent.appendChild(chatItem);
+    }, 1000);
     selectChat(socket);
   });
 
